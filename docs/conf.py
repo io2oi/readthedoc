a2200c165d3d95370468e9dbacb5a7fe6cfe1711
Byung-Chul Lee <io2oi@yahoo.co.kr>
2016-09-09 06:47:54 +0900

    comment out the matplotlib due to building error at readthedoc site

--- docs/conf.py
+++ docs/conf.py
@@ -41,15 +41,23 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+# extensions = [
+#     'sphinx.ext.mathjax',
+#     'matplotlib.sphinxext.only_directives',
+#     'matplotlib.sphinxext.plot_directive',
+#     'sphinx.ext.autodoc',
+#     'sphinx.ext.doctest',
+#     'sphinx.ext.inheritance_diagram',
+#     'numpydoc'
+# ]
 extensions = [
     'sphinx.ext.mathjax',
-    'matplotlib.sphinxext.only_directives',
-    'matplotlib.sphinxext.plot_directive',
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.inheritance_diagram',
     'numpydoc'
 ]
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
